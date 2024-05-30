2457bf4fab256e48f6bbcc0e228d084172ebc8b9
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-05-30 06:40:20 +0200

    add  views

--- dofuspoly/monopoly/views.py
+++ dofuspoly/monopoly/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated 
 
-# Create your views here.
+from .models import Board, Color, Case, Game, Rent, CaseType, Card, CardType, Player
+from .serializers import BoardSerializer, ColorSerializer, CaseSerializer, GameSerializer, RentSerializer, CaseTypeSerializer, CardSerializer, CardTypeSerializer, PlayerSerializer
+
+class BoardViewSet(viewsets.ModelViewSet):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = [IsAuthenticated]
+
+class ColorViewSet(viewsets.ModelViewSet):
+    queryset = Color.objects.all()
+    serializer_class = ColorSerializer
+    permission_classes = [IsAuthenticated]
+
+class CaseViewSet(viewsets.ModelViewSet):
+    queryset = Case.objects.all()
+    serializer_class = CaseSerializer
+    permission_classes = [IsAuthenticated]
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [IsAuthenticated]
+
+class RentViewSet(viewsets.ModelViewSet):
+    queryset = Rent.objects.all()
+    serializer_class = RentSerializer
+    permission_classes = [IsAuthenticated]
+
+class CaseTypeViewSet(viewsets.ModelViewSet):
+    queryset = CaseType.objects.all()
+    serializer_class = CaseTypeSerializer
+    permission_classes = [IsAuthenticated]
+
+class CardViewSet(viewsets.ModelViewSet):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+    permission_classes = [IsAuthenticated]
+
+class CardTypeViewSet(viewsets.ModelViewSet):
+    queryset = CardType.objects.all()
+    serializer_class = CardTypeSerializer
+    permission_classes = [IsAuthenticated]
+
+class PlayerViewSet(viewsets.ModelViewSet): 
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+    permission_classes = [IsAuthenticated]
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+    permission_classes = [IsAuthenticated]

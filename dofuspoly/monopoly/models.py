78ed70c7b23470003d08f411f320b5464a72dd76
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-05-30 06:50:55 +0200

    feat: Update current_player foreign key in Game model

--- dofuspoly/monopoly/models.py
+++ dofuspoly/monopoly/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Game(models.Model):
     board = models.ForeignKey('Board', on_delete=models.CASCADE)
     players = models.ManyToManyField('Player')
-    current_player = models.ForeignKey('Player', null=True, blank=True)
+    current_player = models.ForeignKey('Player', null=True, blank=True, on_delete=models.PROTECT)
     turn = models.IntegerField(default=0)
     finished = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)

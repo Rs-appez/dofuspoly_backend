f60f937bbfe35fcf5dbfe20a752345653781aa74
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-05-30 06:40:36 +0200

    feat: Add rest_framework and monopoly apps to INSTALLED_APPS
    The code changes include adding the 'rest_framework' and 'monopoly' apps to the INSTALLED_APPS list in the settings.py file. This commit enables the functionality provided by these apps in the Django project.

--- dofuspoly/dofuspoly/settings.py
+++ dofuspoly/dofuspoly/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'monopoly',
 ]
 
 MIDDLEWARE = [
@@ -105,11 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Brussels'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
+
+USE_TZ = True   
 
 
 # Static files (CSS, JavaScript, Images)

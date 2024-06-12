8358564aa96345d448224178a3f9d398b21b6d53
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-06-12 10:32:34 +0200

    setup admin page

--- dofuspoly/monopoly/admin.py
+++ dofuspoly/monopoly/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Player, Board, Case, Color, Rent, CaseType, Card, CardType
+
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(Board)
+admin.site.register(Case)
+admin.site.register(Color)
+admin.site.register(Rent)
+admin.site.register(CaseType)
+admin.site.register(Card)
+admin.site.register(CardType)
+

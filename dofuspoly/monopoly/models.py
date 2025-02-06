3dbea207caecc92c60def8da3e5784dbb6ab3c10
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2025-02-06 18:50:16 +0100

    add endpoint roll dice

--- dofuspoly/monopoly/models.py
+++ dofuspoly/monopoly/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+import random
+
+
 class Game(models.Model):
-    board = models.ForeignKey('Board', on_delete=models.CASCADE)
-    players = models.ManyToManyField('Player')
-    current_player = models.ForeignKey('Player', null=True, blank=True, on_delete=models.PROTECT, related_name='current_player')
+    board = models.ForeignKey("Board", on_delete=models.CASCADE)
+    players = models.ManyToManyField("Player")
+    current_player = models.ForeignKey(
+        "Player",
+        null=True,
+        blank=True,
+        on_delete=models.PROTECT,
+        related_name="current_player",
+    )
     turn = models.IntegerField(default=0)
     finished = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,16 +20,23 @@ class Game(models.Model):
     def __str__(self):
         return str(self.id)
 
+    def roll_dice(self):
+        dice1 = random.randint(1, 6)
+
+        self.current_player.move(dice1)
+
+        return dice1
+
+
 class Board(models.Model):
-    cases = models.ManyToManyField('Case')
+    cases = models.ManyToManyField("Case")
 
     def make_board(self):
         self.cases.clear()
         for case in Case.objects.all():
             self.cases.add(case)
-        
-        self.save()
 
+        self.save()
 
 
 class Color(models.Model):
@@ -28,19 +44,20 @@ class Color(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 
 class Case(models.Model):
     name = models.CharField(max_length=255)
     position = models.IntegerField()
     price = models.IntegerField()
-    type = models.ForeignKey('CaseType', on_delete=models.PROTECT)
-    color = models.ForeignKey('Color', null=True, blank=True, on_delete=models.PROTECT)
-    rent = models.ForeignKey('Rent', null=True, blank=True, on_delete=models.DO_NOTHING)
+    type = models.ForeignKey("CaseType", on_delete=models.PROTECT)
+    color = models.ForeignKey("Color", null=True, blank=True, on_delete=models.PROTECT)
+    rent = models.ForeignKey("Rent", null=True, blank=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.name
-    
+
+
 class Rent(models.Model):
     price_house = models.IntegerField()
     mortage = models.IntegerField()
@@ -51,44 +68,54 @@ class Rent(models.Model):
     rent_4_houses = models.IntegerField()
     rent_hotel = models.IntegerField()
 
-class CaseType(models.Model):  
+
+class CaseType(models.Model):
     type = models.CharField(max_length=255)
 
     def __str__(self):
         return self.type
 
+
 class Card(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    type = models.ForeignKey('CardType', on_delete=models.CASCADE)
+    type = models.ForeignKey("CardType", on_delete=models.CASCADE)
 
-    image = models.ImageField(upload_to='statics/cards/')
-    image_back = models.ImageField(upload_to='statics/cards/', null=True, blank=True)
+    image = models.ImageField(upload_to="statics/cards/")
+    image_back = models.ImageField(upload_to="statics/cards/", null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
+
 class CardType(models.Model):
     type = models.CharField(max_length=255)
 
     def __str__(self):
         return self.type
 
+
 class Player(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
     money = models.IntegerField()
     position = models.IntegerField()
     in_jail = models.BooleanField(default=False)
     jail_turns = models.IntegerField(default=0)
-    cards = models.ManyToManyField('Card', blank=True)
-    ownedCase = models.ManyToManyField('OwnedCase', blank=True)
-    image = models.CharField(max_length=255, default='default.png')
+    cards = models.ManyToManyField("Card", blank=True)
+    ownedCase = models.ManyToManyField("OwnedCase", blank=True)
+    image = models.CharField(max_length=255, default="default.png")
 
     def __str__(self):
         return self.user.username
 
+    def move(self, steps):
+        self.position += steps
+        self.position = self.position % 40
+        self.save()
+
+
 class OwnedCase(models.Model):
-    case = models.ForeignKey('Case', on_delete=models.CASCADE)
+    case = models.ForeignKey("Case", on_delete=models.CASCADE)
     mortgaged = models.BooleanField(default=False)
     houses = models.IntegerField(default=0)
     hotel = models.BooleanField(default=False)
--- dofuspoly/monopoly/views.py
+++ dofuspoly/monopoly/views.py
@@ -4,56 +4,81 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Board, Color, Case, Game, Rent, CaseType, Card, CardType, Player
-from .serializers import BoardSerializer, ColorSerializer, CaseSerializer, GameSerializer, RentSerializer, CaseTypeSerializer, CardSerializer, CardTypeSerializer, PlayerSerializer
+from .serializers import (
+    BoardSerializer,
+    ColorSerializer,
+    CaseSerializer,
+    GameSerializer,
+    RentSerializer,
+    CaseTypeSerializer,
+    CardSerializer,
+    CardTypeSerializer,
+    PlayerSerializer,
+)
+
 
 class BoardViewSet(viewsets.ModelViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
     permission_classes = [IsAuthenticated]
 
-    @action(detail=False, methods=['post'], permission_classes=[IsAdminUser])
+    @action(detail=False, methods=["post"], permission_classes=[IsAdminUser])
     def make_board(self, request, pk=None):
         board = Board()
         board.save()
         board.make_board()
-        return Response({'status': 'board created'})
-    
+        return Response({"status": "board created"})
+
+
 class ColorViewSet(viewsets.ModelViewSet):
     queryset = Color.objects.all()
     serializer_class = ColorSerializer
     permission_classes = [IsAuthenticated]
+
 
 class CaseViewSet(viewsets.ModelViewSet):
     queryset = Case.objects.all()
     serializer_class = CaseSerializer
     permission_classes = [IsAuthenticated]
 
+
 class GameViewSet(viewsets.ModelViewSet):
     queryset = Game.objects.all()
     serializer_class = GameSerializer
     permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=["post"], permission_classes=[IsAdminUser])
+    def roll_dice(self, request, pk=None):
+        game = self.get_object()
+        result = game.roll_dice()
+        return Response({"status": "dice rolled", "result": result})
+
 
 class RentViewSet(viewsets.ModelViewSet):
     queryset = Rent.objects.all()
     serializer_class = RentSerializer
     permission_classes = [IsAuthenticated]
 
+
 class CaseTypeViewSet(viewsets.ModelViewSet):
     queryset = CaseType.objects.all()
     serializer_class = CaseTypeSerializer
     permission_classes = [IsAuthenticated]
+
 
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
     permission_classes = [IsAuthenticated]
 
+
 class CardTypeViewSet(viewsets.ModelViewSet):
     queryset = CardType.objects.all()
     serializer_class = CardTypeSerializer
     permission_classes = [IsAuthenticated]
 
-class PlayerViewSet(viewsets.ModelViewSet): 
+
+class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
     permission_classes = [IsAuthenticated]

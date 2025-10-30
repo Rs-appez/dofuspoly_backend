bc622d83ef280acdb8691bb434eedef0e24438f1
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-10-30 10:10:38 +0100

    format

--- dofuspoly/dofuspoly/settings.py
+++ dofuspoly/dofuspoly/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [config("FRONTEND_HOST", default="http://localhost:4200")]
 
-CSRF_TRUSTED_ORIGINS = ['https://'+ config('BACKEND_HOST',default='')]
+CSRF_TRUSTED_ORIGINS = ["https://" + config("BACKEND_HOST", default="")]
 
 # Application definition
 

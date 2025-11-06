52777a171a95cc1b92eac82fe27af0db9cdc5a78
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-06 14:39:50 +0100

    feat(realtime): add game update broadcast utility
    Introduced `update_game` function in `realtimes.py` to broadcast game
    updates via Django Channels. This utility serializes the game state and
    sends it to the appropriate group for real-time updates.

---
+++ dofuspoly/monopoly/realtimes.py
@@ -0,0 +1,25 @@
+import json
+
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
+from .models import Game
+from .serializers import GameSerializer
+
+
+def update_game(game: Game) -> None:
+    """
+    Send a ticket to update the game.
+    """
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f"game_{game.id}",
+        {
+            "type": "new_ticks",
+            "content": json.dumps(
+                {
+                    "type": "game_update",
+                    "game": GameSerializer(game).data,
+                }
+            ),
+        },
+    )

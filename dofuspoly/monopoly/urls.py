5fd40736e4bb1b32bce28d34e932d073bdd50b4e
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-06 11:41:57 +0100

    remove old route

--- dofuspoly/monopoly/urls.py
+++ dofuspoly/monopoly/urls.py
@@ -4,13 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'board', views.BoardViewSet)
-router.register(r'color', views.ColorViewSet)
-router.register(r'case', views.CaseViewSet)
 router.register(r'game', views.GameViewSet)
-router.register(r'rent', views.RentViewSet)
-router.register(r'case_type', views.CaseTypeViewSet)
-router.register(r'card', views.CardViewSet)
-router.register(r'card_type', views.CardTypeViewSet)
 router.register(r'player', views.PlayerViewSet)
 
 

8f7039481f028a477648342713a8dc1ea09e143b
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-06 14:16:27 +0100

    add websocket route

--- dofuspoly/dofuspoly/urls.py
+++ dofuspoly/dofuspoly/urls.py
@@ -5,9 +5,13 @@ from rest_framework_simplejwt.views import (
 from django.contrib import admin
 from django.urls import path, include
 
+from monopoly.urls import websocket_urlpatterns as monopoly_websocket_urlpatterns
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("dofuspoly/", include("monopoly.urls")),
 ]
+
+websocket_urlpatterns = monopoly_websocket_urlpatterns

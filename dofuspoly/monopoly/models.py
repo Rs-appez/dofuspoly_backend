f2d755a9c9f19a3759a38529fd18d07b458cb2fb
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-10 16:05:52 +0100

    feat(player): add trigger_space_effect and bankruptcy checks
    Add trigger_space_effect method to Player for handling board space
    effects, including rent, tax, jail, and start bonuses. Refactor money
    updates to use update_money with bankruptcy checks. Prevent turn end if
    player is bankrupt.

--- dofuspoly/monopoly/models.py
+++ dofuspoly/monopoly/models.py
@@ -145,10 +145,20 @@ class Player(models.Model):
         self.has_rolled = False
         self.save()
 
+    def update_money(self, amount: int):
+        self.money += amount
+        self.save()
+
+        if self.money < 0:
+            raise GameException("Player is bankrupt")
+
     @player_turn_required
     def end_turn(self, game: Game):
         if not self.has_rolled:
             raise GameException("You must roll before ending your turn")
+        if self.money < 0:
+            raise GameException("You are bankrupt and cannot end your turn")
+
         game.end_turn()
 
     @player_turn_required
@@ -167,6 +177,38 @@ class Player(models.Model):
         self.save()
 
     @player_turn_required
+    def trigger_space_effect(self, game: Game):
+        space = game.board.spaces.get(position=self.position)
+        if owned_space := game.board.players.owned_spaces.filter(
+            space__position=self.position
+        ).first():
+            rent = owned_space.calculate_rent()
+            self.update_money(-rent)
+            owner = game.players.filter(
+                owned_spaces__space__position=self.position
+            ).first()
+            owner.update_money(rent)
+            owner.save()
+
+        elif space.type.type == "Tax":
+            self.money -= space.price
+        elif space.type.type == "Go to Jail":
+            jail_space = game.board.spaces.get(type__type="Jail")
+            self.position = jail_space.position
+            self.in_jail = True
+            self.jail_turns = 0
+        elif space.type.type == "Chance":
+            pass  # To be implemented
+        elif space.type.type == "Community Chest":
+            pass  # To be implemented
+        elif space.type.type == "Free Parking":
+            pass  # No effect
+        elif space.type.type == "Start":
+            self.update_money(200)
+
+        self.save()
+
+    @player_turn_required
     def buy_space(self, game: Game, space: Space):
         if game.players.filter(owned_spaces__space=space).first():
             raise GameException("This space is already owned")
@@ -176,7 +218,7 @@ class Player(models.Model):
 
         owned_space = OwnedSpace.objects.create(space=space)
         self.owned_spaces.add(owned_space)
-        self.money -= space.price
+        self.update_money(-space.price)
         self.save()
 
 

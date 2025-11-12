d275bb2c0113d31743e4d977e460112aacc72094
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-11-12 14:24:02 +0100

    feat(monopoly): add nb_spaces to Color and rent logic
    Add nb_spaces field to Color model and update OwnedSpace rent calculation
    to double rent when all color spaces are owned and no houses are built.
    Also refactor rent logic for Railroads and Utilities, and add mortgage
    value calculation for OwnedSpace.

--- dofuspoly/monopoly/models/space.py
+++ dofuspoly/monopoly/models/space.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ..exceptions import GameException
 
 
 class Space(models.Model):
@@ -39,6 +40,47 @@ class OwnedSpace(models.Model):
         if self.is_mortgaged:
             return 0
 
+        match self.space.type.type:
+            case "Railroad":
+                return self.__railroad_rent()
+            case "Utility":
+                game = self.player.game
+                dice_total = game.dice1Value + game.dice2Value
+                return self.__utility_rent(dice_total)
+            case "Street":
+                return self.__street_rent()
+            case _:
+                raise GameException("This space type has no rent")
+
+    def __railroad_rent(self) -> int:
+        nb_railroads_owned = self.player.owned_spaces.filter(
+            space__type__type="Railroad"
+        ).count()
+        match nb_railroads_owned:
+            case 1:
+                return 25
+            case 2:
+                return 50
+            case 3:
+                return 100
+            case 4:
+                return 200
+            case _:
+                raise GameException("Invalid number of railroads owned")
+
+    def __utility_rent(self, dice_total: int) -> int:
+        nb_utilities_owned = self.player.owned_spaces.filter(
+            space__type__type="Utility"
+        ).count()
+
+        if nb_utilities_owned == 1:
+            return dice_total * 4
+        elif nb_utilities_owned == 2:
+            return dice_total * 10
+        else:
+            return 0
+
+    def __street_rent(self) -> int:
         rent = self.space.rent.rent
 
         if self.has_hotel:
@@ -53,9 +95,28 @@ class OwnedSpace(models.Model):
             elif self.houses == 4:
                 rent = self.space.rent.rent_4_houses
 
-        # if self.space.type.type == ""
+        nb_color_owned = self.player.owned_spaces.filter(
+            space__color=self.space.color
+        ).count()
+
+        if nb_color_owned == self.space.color.nb_spaces and self.houses == 0:
+            rent *= 2
 
         return rent
+
+    def calculate_mortgage_value(self) -> int:
+        if self.is_mortgaged:
+            raise GameException("Space is already mortgaged")
+
+        match self.space.type.type:
+            case "Railroad":
+                return 100
+            case "Utility":
+                return self.space.price // 2
+            case "Street":
+                return self.space.rent.mortage
+            case _:
+                raise GameException("This space type cannot be mortgaged")
 
 
 class Rent(models.Model):
@@ -71,6 +132,7 @@ class Rent(models.Model):
 
 class Color(models.Model):
     name = models.CharField(max_length=255)
+    nb_spaces = models.IntegerField(default=3)
 
     def __str__(self):
         return self.name
